fcbc482c24fd3198c03c03840e1636f9eb7697f7
dipeshtech <dipesh.nepali@gmail.com>
2020-08-01 10:08:28 +0545

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <center><h1>Welcome to your Online Abacus Class!</h1></center>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/dipeshtimilsina/AbacusClass"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         <h2>Check out our project!</h2> 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
